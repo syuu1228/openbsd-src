c873644237e8b9efcb47e88c2e6a287d5d1f4384
briggs <briggs>
1997-01-24 01:35:54 +0000

    Sync w/ NETBSD_CURRENT_971122.

--- sys/arch/mac68k/dev/obiovar.h
+++ sys/arch/mac68k/dev/obiovar.h
@@ -1,11 +1,11 @@
-/*	$OpenBSD: src/sys/arch/mac68k/dev/obio.c,v 1.2 1997/01/24 01:35:36 briggs Exp $	*/
-/*	$NetBSD: obio.c,v 1.5 1996/12/17 06:47:37 scottr Exp $	*/
+/*	$OpenBSD: src/sys/arch/mac68k/dev/obiovar.h,v 1.1 1997/01/24 01:35:37 briggs Exp $	*/
+/*	$NetBSD: obiovar.h,v 1.1 1996/12/17 06:47:38 scottr Exp $	*/
 
 /*
  * Copyright (c) 1996 The NetBSD Foundation, Inc.
  * All rights reserved.
  *
- * This code is derived from software contributed to the NetBSD Foundation
+ * This code is derived from software contributed to The NetBSD Foundation
  * by Jason R. Thorpe.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -37,76 +37,14 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <sys/param.h>
-#include <sys/systm.h>
-#include <sys/device.h>
+/*
+ * Autoconfiguration definitions and prototypes for the mac68k
+ * on-board i/o space.
+ */
 
-#include <mac68k/dev/obiovar.h>
-
-static int	obio_match __P((struct device *, struct cfdata *, void *));
-static void	obio_attach __P((struct device *, struct device *, void *));
-static int	obio_print __P((void *, const char *));
-static int	obio_search __P((struct device *, struct cfdata *, void *));
-
-struct cfattach obio_ca = {
-	sizeof(struct device), obio_match, obio_attach
+/*
+ * Arguments used to attach a device to the internal i/o space.
+ */
+struct obio_attach_args {
+	caddr_t oa_addr;		/* physical address */
 };
-
-struct cfdriver obio_cd = {
-	NULL, "obio", DV_DULL
-};
-
-static int
-obio_match(parent, cf, aux)
-	struct device *parent;
-	struct cfdata *cf;
-	void *aux;
-{
-	static int obio_matched = 0;
-
-	/* Allow only one instance. */
-	if (obio_matched)
-		return (0);
-
-	obio_matched = 1;
-	return (1);
-}
-
-static void
-obio_attach(parent, self, aux)
-	struct device *parent;
-	struct device *self;
-	void *aux;
-{
-	printf("\n");
-
-	/* Search for and attach children. */
-	(void) config_search(obio_search, self, aux);
-}
-
-int
-obio_print(args, name)
-	void *args;
-	const char *name;
-{
-	struct obio_attach_args *oa = (struct obio_attach_args *) args;
-
-	if (oa->oa_addr)
-		printf(" addr %p", oa->oa_addr);
-
-	return (UNCONF);
-}
-
-int
-obio_search(parent, cf, aux)
-	struct device *parent;
-	struct cfdata *cf;
-	void *aux;
-{
-	struct obio_attach_args oa;
-
-	bzero(&oa, sizeof(oa));
-	if ((*cf->cf_attach->ca_match)(parent, cf, &oa) > 0)
-		config_attach(parent, cf, &oa, obio_print);
-	return (0);
-}
